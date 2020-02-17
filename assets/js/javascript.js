da92b660a73b92c0193ba60d165f15e2cb07d9b8
mekaleka <www.no-reply.com>
2020-02-16 22:33:03 -0700

    Setting up variables for APIKey and queryURL and ajax call

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,22 @@
 
+// Weather Homework URLS: 
+// Current Weather:  "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial"
+// Forecast: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial"
+// Icon: "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
+
 
 // API Key
 // 6fa0cfaeeef5ac6923a4ec8f2b209eff
+
+
+
+var APIKey =
+
+var queryURL =
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+
+});
